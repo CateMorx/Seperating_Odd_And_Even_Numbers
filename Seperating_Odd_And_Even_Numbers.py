9f1f2d775442a70b6ecb25670151ab2dab5cb170
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-04-22 21:43:50 +0800

    pseudocodes and text file

---
+++ Seperating_Odd_And_Even_Numbers.py
@@ -0,0 +1,33 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 3 
+#Problem 1: Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+#The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt.
+# The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+
+#Imports necessary elements
+#Creates Method For Separating Odd and Even Numbers From A text file containing 20 integers
+#Opens and creates the 3 files, "numbers.txt","even.txt","odd.txt"
+#Reads each line in the "numbers.txt"
+#Determines if Even number and appends the value unto the "even.txt" file
+#Determines if Odd number and appends the value unto the "odd.txt" file
+
+#Creates a Method to Display the contents of the "numbers.txt" file using GUI
+#Opens and reads the "numbers.txt" file
+# Create the PySimpleGUI window
+ # Loop indefinitely to create the animated gradient effect
+
+#Creates a Method to Display the contents of the "even.txt" file using GUI
+#Opens and reads the "even.txt" file
+# Create the PySimpleGUI window
+ # Loop indefinitely to create the animated gradient effect
+
+#Creates a Method to Display the contents of the "odd.txt" file using GUI
+#Opens and reads the "odd.txt" file
+# Create the PySimpleGUI window
+ # Loop indefinitely to create the animated gradient effect
+
+#Calls the proccess method
+#Calls the First GUI method
+#Calls the Second GUI method
+#Calls the Third GUI method
